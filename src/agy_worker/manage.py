1084aad69050ccd05bb02a3ad68b42545bab807e
Elio-ZWD <z704554363@gmail.com>
2026-09-10 23:03:22 +0800

    fix: 生成 MCP 瘦请求 Schema

--- src/agy_worker/manage.py
+++ src/agy_worker/manage.py
@@ -12,7 +12,7 @@ import tomlkit
 from .common import WorkerError, atomic_json, digest
 from .controller_client import ControllerClient
 from .controller_protocol import PROTOCOL_VERSION
-from .models import (WorkerRequest,ContinueRequest,StatusRequest,CancelRequest,
+from .models import (McpWorkerRequest,McpContinueRequest,StatusRequest,CancelRequest,
                      ArtifactRequest,CapabilitiesRequest)
 from .security import inspect_data_dir_acl
 
@@ -135,7 +135,7 @@ def doctor():
 
 
 def schemas():
-    for name,model in [('agy_capabilities',CapabilitiesRequest),('agy_worker',WorkerRequest),('agy_continue',ContinueRequest),('agy_status',StatusRequest),('agy_cancel',CancelRequest),('agy_artifact_read',ArtifactRequest)]:
+    for name,model in [('agy_capabilities',CapabilitiesRequest),('agy_worker',McpWorkerRequest),('agy_continue',McpContinueRequest),('agy_status',StatusRequest),('agy_cancel',CancelRequest),('agy_artifact_read',ArtifactRequest)]:
         atomic_json(ROOT/'schemas'/(name+'.json'),model.model_json_schema(by_alias=True))
 
 
